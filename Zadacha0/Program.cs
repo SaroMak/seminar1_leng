a61786018cf46e3227bf4d5a795e98ab55af57f4
«Saro» <opac4321@mail.ru>
2022-11-23 22:32:40 +0300

    первая задача

---
+++ Zadacha0/Program.cs
@@ -0,0 +1,5 @@
+﻿Console.Clear();
+Console.Write("Введите число: ");
+int number  = int.Parse (Console.ReadLine());
+int square =number * number;
+Console.WriteLine($" квадрат числа {number} равен {square} ");
